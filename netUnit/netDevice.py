3c20f869ba405addf0d53d0a0ac70714afd4db3b
MakHome <sergeyklimets@gmail.com>
2021-02-24 01:56:30 +0300

    init

---
+++ netUnit/netDevice.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+from ipaddress import *
+
+
+class NetDevice:
+    """Прототип устройства"""
+
+    def __init__(self, ip, comunity):
+        self.__ip = ip
+        self.__comunity = comunity
+
+    @property
+    def ip(self):
+        return self.__ip
+
+    @ip.setter
+    def ip(self, i):
+        try:
+            ip_address(i)
+        except ValueError:
+            raise ValueError
+        else:
+            self.__ip = i
